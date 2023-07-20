be3706cebec15adac413f41fe71c37236d11afa6
alperenbicak <alperenbicak3@gmail.com>
2023-07-20 09:24:11 +0300

    update

---
+++ MerkezBankasıRestApi/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿
+global using Microsoft.EntityFrameworkCore;
+using MerkezBankasıRestApi.Kurlar;
+using System.Collections.Generic;
+namespace MerkezBankasıRestApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=MerkezBankasi;Trusted_Connection=true;TrustServerCertificate=true;");
+
+        }
+        public DbSet<ResponseDataKur> Response { get; set; }
+    }
+}
+
---
+++ merkezbankasi/Models.cs
@@ -0,0 +1,3 @@
+﻿string tcmblink = string.Format("https://www.tcmb.gov.tr/kurlar/{0}.xml", (request.IsBugun) ? ("today") : string.Format("{2}{1}/{0}{1}{2}"
+                   , request.Gun.ToString(), request.Ay.ToString(), request.Yil));
+Console.WriteLine();
